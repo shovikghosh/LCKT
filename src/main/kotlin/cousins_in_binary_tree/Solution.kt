81996981c08d95ed90dc19430d35dee478040fdd
shovikghosh <ghosh.shovik@gmail.com>
2021-10-20 14:50:55 +0530

    gitignore

--- src/main/kotlin/cousins_in_binary_tree/Solution.kt
+++ src/main/kotlin/cousins_in_binary_tree/Solution.kt
@@ -1,5 +1,7 @@
 package cousins_in_binary_tree
 
+import kotlin.math.max
+
 /**
  * Example:
  * var ti = TreeNode(5)
@@ -11,8 +13,30 @@ package cousins_in_binary_tree
  * }
  */
 class Solution {
+
     fun isCousins(root: TreeNode?, x: Int, y: Int): Boolean {
-        return true
+        val a1 = ancestor(root, x)
+        val a2 = ancestor(root, y)
+        return if(a1 == a2) false
+        else depth(root, x) == depth(root, y)
+    }
+
+    fun depth(root: TreeNode?, value: Int) : Int {
+        return if (root == null) Int.MIN_VALUE
+        else if (root.`val` == value) 0
+        else 1 + max(depth(root.left, value), depth(root.right, value))
+    }
+
+    fun ancestor(root: TreeNode?, value: Int) : TreeNode? {
+        return if (root == null) null
+        else if (root.left?.`val` == value || root.right?.`val` == value) root
+        else {
+            val l = ancestor(root.left, value)
+            val r = ancestor(root.right, value)
+            if (l== null && r == null) null
+            else if (r == null) l
+            else r
+        }
     }
 }
 
--- src/test/kotlin/cousins_in_binary_tree/SolutionTest.kt
+++ src/test/kotlin/cousins_in_binary_tree/SolutionTest.kt
@@ -1,4 +1,41 @@
 package cousins_in_binary_tree
 
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class SolutionTest {
+    private val n1 = TreeNode(1)
+    private val n2 = TreeNode(2)
+    private val n3 = TreeNode(3)
+    private val n4 = TreeNode(4)
+    private val n5 = TreeNode(5)
+
+    @BeforeEach
+    fun setUp() {
+        n1.left = n2
+        n1.right = n3
+        n2.right = n4
+        n3.right = n5
+    }
+
+    @Test
+    fun testDepth() {
+        assertEquals(Solution().depth(n1, n3.`val`),1, "WTH!!")
+    }
+
+    @Test
+    fun testAncestor() {
+        val res = Solution().ancestor(n1, 4)
+        assertEquals(res?.`val`, 2, "WTYY!")
+    }
+
+    @Test
+    fun testIsCousin() {
+        val res = Solution().isCousins(n1, 5 , 4)
+        assertEquals(true, res, "JHGK!")
+    }
+
+
+
 }
